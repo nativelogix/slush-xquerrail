51f77932d55552c9b75724a517d6cf424cde2981
Richard Louapre <richard.louapre@marklogic.com>
2015-06-11 12:38:31 -0400

    Fix XQuerrail application name
    It was hardcoded.

--- generators/app.js
+++ generators/app.js
@@ -8,161 +8,168 @@
 
 module.exports = function(gulp, common, modules) {
 
-    function validateNamespace(namespace) {
-        if (! modules['_.string'].contains(namespace, ':')) {
-            return 'Invalid namespace [' + namespace + ']. Must have the format prefix:namespace-uri';
-        } else
-            return true;
-    };
+  function validateNamespace(namespace) {
+    if (! modules['_.string'].contains(namespace, ':')) {
+      return 'Invalid namespace [' + namespace + ']. Must have the format prefix:namespace-uri';
+    } else
+    return true;
+  };
 
-    function format(string) {
-        var username = string.toLowerCase();
-        return username.replace(/\s/g, '');
+  function format(string) {
+    var username = string.toLowerCase();
+    return username.replace(/\s/g, '');
+  }
+
+  var defaults = (function () {
+    var workingDirName = modules.path.basename(process.cwd()),
+    homeDir, appDescription, osUserName, configFile, user;
+
+    if (process.platform === 'win32') {
+      homeDir = process.env.USERPROFILE;
+      osUserName = process.env.USERNAME || modules.path.basename(homeDir).toLowerCase();
+    }
+    else {
+      homeDir = process.env.HOME || process.env.HOMEPATH;
+      osUserName = homeDir && homeDir.split('/').pop() || 'root';
     }
 
-    var defaults = (function () {
-        var workingDirName = modules.path.basename(process.cwd()),
-          homeDir, appDescription, osUserName, configFile, user;
+    configFile = modules.path.join(homeDir, '.gitconfig');
+    user = {};
 
-        if (process.platform === 'win32') {
-            homeDir = process.env.USERPROFILE;
-            osUserName = process.env.USERNAME || modules.path.basename(homeDir).toLowerCase();
+    if (require('fs').existsSync(configFile)) {
+      user = require('iniparser').parseSync(configFile).user;
+    }
+
+    var applicationNamespace, contentNamespace, collation, domain, package;
+    package = common.package;
+
+    if (package) {
+      appDescription = common.package.description;
+    } else {
+      appDescription = modules.inflection.titleize(workingDirName);
+    }
+
+    domain = common.domain.find('application');
+    if (domain) {
+      applicationNamespace = domain.namespace.prefix + ':' + domain.namespace.uri;
+    } else {
+      applicationNamespace = common.default.namespaces.application.prefix + ':' + common.default.namespaces.application.uri;
+    }
+
+    domain = common.domain.find('content');
+    if (domain) {
+      contentNamespace = domain.namespace.prefix + ':' + domain.namespace.uri;
+    } else {
+      contentNamespace = common.default.namespaces.content.prefix + ':' + common.default.namespaces.content.uri;
+    }
+
+    collation = common.configuration.get('application:collation') || common.default.collation;
+
+    return {
+      appName: workingDirName,
+      appDescription: appDescription,
+      userName: osUserName || format(user.name || ''),
+      authorName: user.name || '',
+      authorEmail: user.email || '',
+      xquerrail: {
+        applicationNamespace: applicationNamespace,
+        contentNamespace: contentNamespace,
+        collation: collation
+      }
+    };
+  })();
+
+  gulp.task('default', function (done) {
+    var prompts = [{
+      name: 'appName',
+      message: 'What is the name of your project?',
+      default: defaults.appName
+    }, {
+      name: 'appDescription',
+      message: 'What is the description or your project?',
+      default: defaults.appDescription
+    }, {
+      name: 'appVersion',
+      message: 'What is the version of your project?',
+      default: '0.1.0'
+    }, {
+      name: 'xquerrailVersion',
+      message: 'XQuerrail version?',
+      default: common.default.xquerrail.version
+    }, {
+      name: 'xquerrailApplicationName',
+      message: 'What is XQuerrail application name?',
+      default: 'app',
+      validate: common.validation.required
+    }, {
+      name: 'xquerrailApplicationNamespace',
+      message: 'What is the application namespace? Supported format {prefix}:{namespace}',
+      default: defaults.xquerrail.applicationNamespace,
+      validate: validateNamespace
+    }, {
+      name: 'xquerrailContentNamespace',
+      message: 'What is the content namespace? Supported format {prefix}:{namespace}',
+      default: defaults.xquerrail.contentNamespace,
+      validate: validateNamespace
+    }, {
+      name: 'xquerrailCollation',
+      message: 'What is the application collation?',
+      default: defaults.xquerrail.collation
+    }, {
+      name: 'authorName',
+      message: 'What is the author name?',
+      default: defaults.authorName
+    }, {
+      name: 'authorEmail',
+      message: 'What is the author email?',
+      default: defaults.authorEmail
+    }, {
+      type: 'confirm',
+      name: 'moveon',
+      message: 'Continue?'
+    }];
+
+    //Ask
+    modules.inquirer.prompt(prompts,
+      function (answers) {
+        if (!answers.moveon) {
+          return done();
         }
-        else {
-            homeDir = process.env.HOME || process.env.HOMEPATH;
-            osUserName = homeDir && homeDir.split('/').pop() || 'root';
-        }
-
-        configFile = modules.path.join(homeDir, '.gitconfig');
-        user = {};
-
-        if (require('fs').existsSync(configFile)) {
-            user = require('iniparser').parseSync(configFile).user;
-        }
-
-        var applicationNamespace, contentNamespace, collation, domain, package;
-        package = common.package;
-
-        if (package) {
-            appDescription = common.package.description;
-        } else {
-            appDescription = modules.inflection.titleize(workingDirName);
-        }
-
-        domain = common.domain.find('application');
-        if (domain) {
-            applicationNamespace = domain.namespace.prefix + ':' + domain.namespace.uri;
-        } else {
-            applicationNamespace = common.default.namespaces.application.prefix + ':' + common.default.namespaces.application.uri;
-        }
-        
-        domain = common.domain.find('content');
-        if (domain) {
-            contentNamespace = domain.namespace.prefix + ':' + domain.namespace.uri;
-        } else {
-            contentNamespace = common.default.namespaces.content.prefix + ':' + common.default.namespaces.content.uri;
-        }
-
-        collation = common.configuration.get('application:collation') || common.default.collation;
-
-        return {
-            appName: workingDirName,
-            appDescription: appDescription,
-            userName: osUserName || format(user.name || ''),
-            authorName: user.name || '',
-            authorEmail: user.email || '',
-            xquerrail: {
-                applicationNamespace: applicationNamespace,
-                contentNamespace: contentNamespace,
-                collation: collation
-            }
+        answers.appNameSlug = modules['_.string'].slugify(answers.appName);
+        answers.xquerrail = {};
+        answers.xquerrail.name = answers.xquerrailApplicationName;
+        // delete answers.xquerrailApplicationName;
+        answers.xquerrail.user = answers.appNameSlug + '-user';
+        answers.xquerrail.applicationNamespace = {
+          'prefix': answers.xquerrailApplicationNamespace.substring(0, answers.xquerrailApplicationNamespace.indexOf(':')),
+          'uri': answers.xquerrailApplicationNamespace.substring(answers.xquerrailApplicationNamespace.indexOf(':') + 1)
         };
-    })();
-
-    gulp.task('default', function (done) {
-        var prompts = [{
-            name: 'appName',
-            message: 'What is the name of your application?',
-            default: defaults.appName
-        }, {
-            name: 'appDescription',
-            message: 'What is the description?',
-            default: defaults.appDescription
-        }, {
-            name: 'appVersion',
-            message: 'What is the version of your project?',
-            default: '0.1.0'
-        }, {
-            name: 'xquerrailVersion',
-            message: 'XQuerrail version?',
-            default: common.default.xquerrail.version
-        }, {
-            name: 'xquerrailApplicationNamespace',
-            message: 'What is the application namespace? Supported format {prefix}:{namespace}',
-            default: defaults.xquerrail.applicationNamespace,
-            validate: validateNamespace
-        }, {
-            name: 'xquerrailContentNamespace',
-            message: 'What is the content namespace? Supported format {prefix}:{namespace}',
-            default: defaults.xquerrail.contentNamespace,
-            validate: validateNamespace
-        }, {
-            name: 'xquerrailCollation',
-            message: 'What is the application collation?',
-            default: defaults.xquerrail.collation
-        }, {
-            name: 'authorName',
-            message: 'What is the author name?',
-            default: defaults.authorName
-        }, {
-            name: 'authorEmail',
-            message: 'What is the author email?',
-            default: defaults.authorEmail
-        // }, {
-        //     name: 'userName',
-        //     message: 'What is the github username?',
-        //     default: defaults.userName
-        }, {
-            type: 'confirm',
-            name: 'moveon',
-            message: 'Continue?'
-        }];
-        //Ask
-        modules.inquirer.prompt(prompts,
-            function (answers) {
-                if (!answers.moveon) {
-                    return done();
-                }
-                answers.appNameSlug = modules['_.string'].slugify(answers.appName);
-                answers.xquerrail = {};
-                answers.xquerrail.name = answers.appNameSlug;
-                answers.xquerrail.user = answers.xquerrail.name + '-user';
-                answers.xquerrail.applicationNamespace = {
-                    'prefix': answers.xquerrailApplicationNamespace.substring(0, answers.xquerrailApplicationNamespace.indexOf(':')),
-                    'uri': answers.xquerrailApplicationNamespace.substring(answers.xquerrailApplicationNamespace.indexOf(':') + 1)
-                };
-                delete answers.xquerrailApplicationNamespace;
-                answers.xquerrail.contentNamespace = {
-                    'prefix': answers.xquerrailContentNamespace.substring(0, answers.xquerrailContentNamespace.indexOf(':')),
-                    'uri': answers.xquerrailContentNamespace.substring(answers.xquerrailContentNamespace.indexOf(':') + 1)
-                };
-                delete answers.xquerrailContentNamespace;
-                answers.xquerrail.collation = answers.xquerrailCollation;
-                delete answers.xquerrailCollation;
-                common.dependencies.xray.setup(
-                    answers,
-                    function() {
-                        gulp.src(__dirname + '/../templates/app/**')
-                            .pipe(modules.template(answers))
-                            .pipe(modules.conflict('./'))
-                            .pipe(gulp.dest('./'))
-                            .pipe(modules.install())
-                            .on('finish', function () {
-                                console.log('FINISHED');
-                                done();
-                            });
-                });
+        delete answers.xquerrailApplicationNamespace;
+        answers.xquerrail.contentNamespace = {
+          'prefix': answers.xquerrailContentNamespace.substring(0, answers.xquerrailContentNamespace.indexOf(':')),
+          'uri': answers.xquerrailContentNamespace.substring(answers.xquerrailContentNamespace.indexOf(':') + 1)
+        };
+        delete answers.xquerrailContentNamespace;
+        answers.xquerrail.collation = answers.xquerrailCollation;
+        delete answers.xquerrailCollation;
+        gulp.src(__dirname + '/../templates/app/**')
+        .pipe(modules.template(answers))
+        .pipe(modules.rename(function (file) {
+          file.dirname = modules['_'].template(file.dirname)(answers);
+          file.basename = modules['_'].template(file.basename)(answers);
+          console.log(file);
+        }))
+        .pipe(modules.conflict('./'))
+        .pipe(gulp.dest('./'))
+        .pipe(modules.install())
+        .on('finish', function () {
+          common.dependencies.xray.setup(
+            answers,
+            function() {
+              done();
             });
-    });
+        });
+      });
+  });
   return gulp;
 }
--- generators/view.js
+++ generators/view.js
@@ -8,96 +8,96 @@
 
 module.exports = function(gulp, common, modules) {
 
-var listControllers = function(answers) {
+  var listControllers = function(answers) {
     return common.domain.find(answers.domainName || common.domain.default).controllers;
-};
+  };
 
-var _answers = {
+  var _answers = {
     'domainName': common.domain.default
-};
+  };
 
-var validateAndCapture = function(input, key) {
+  var validateAndCapture = function(input, key) {
     var required = common.validation.required(input);
     if (required === true) {
-        _answers[key] = input
+      _answers[key] = input
     }
     return required;
-};
+  };
 
-var validateActionName = function(input) {
+  var validateActionName = function(input) {
     var required = common.validation.required(input);
     if (required === true) {
-        var domain = common.domain.find(_answers['domainName'])
-        var view;
-        var action;
-        domain.views.some(function(v) {
-            if (v.controller === _answers['controllerName']) {
-                view = v;
-                return true;
-            }
+      var domain = common.domain.find(_answers['domainName'])
+      var view;
+      var action;
+      domain.views.some(function(v) {
+        if (v.controller === _answers['controllerName']) {
+          view = v;
+          return true;
+        }
+      });
+      if (view) {
+        view.actions.some(function(a) {
+          if (a.name === input && modules['_.string'].contains(a.formats, _answers['viewFormat'])) {
+            action = a;
+            return true;
+          }
         });
-        if (view) {
-            view.actions.some(function(a) {
-                if (a.name === input && modules['_.string'].contains(a.formats, _answers['viewFormat'])) {
-                    action = a;
-                    return true;
-                }
-            });
-        }
-        if (action) {
-            return 'View name [controller-name.action-name.format] must be unique.'
-        } else {
-            return true
-        }
+      }
+      if (action) {
+        return 'View name [controller-name.action-name.format] must be unique.'
+      } else {
+        return true
+      }
     } else {
-        return required;
+      return required;
     }
-};
+  };
 
-gulp.task('view', function (done) {
+  gulp.task('view', function (done) {
     var prompts = [{
-        name: 'domainName',
-        type: 'list',
-        message: 'What is the domain model?',
-        choices: common.domain.list,
-        default: common.domain.default,
-        when: function() {return false;},
-        validate: function(input) { return validateAndCapture(input, 'domainName')}
+      name: 'domainName',
+      type: 'list',
+      message: 'What is the domain model?',
+      choices: common.domain.list,
+      default: common.domain.default,
+      when: function() {return false;},
+      validate: function(input) { return validateAndCapture(input, 'domainName')}
     }, {
-        type: 'list',
-        name: 'controllerName',
-        message: 'Controller name in view?',
-        choices: listControllers,
-        filter: function(input) { validateAndCapture(input, 'controllerName'); return input;}
+      type: 'list',
+      name: 'controllerName',
+      message: 'Controller name in view?',
+      choices: listControllers,
+      filter: function(input) { validateAndCapture(input, 'controllerName'); return input;}
     }, {
-        type: 'checkbox',
-        name: 'viewFormat',
-        message: 'Format in view?',
-        choices: common.domain.view.formats,
-        validate: function(input) { return validateAndCapture(input, 'viewFormat')}
+      type: 'checkbox',
+      name: 'viewFormat',
+      message: 'Format in view?',
+      choices: common.domain.view.formats,
+      validate: function(input) { return validateAndCapture(input, 'viewFormat')}
     }, {
-        name: 'actionName',
-        message: 'Action name?',
-        validate: validateActionName
+      name: 'actionName',
+      message: 'Action name?',
+      validate: validateActionName
     }, {
-        type: 'confirm',
-        name: 'moveon',
-        message: 'Continue?'
+      type: 'confirm',
+      name: 'moveon',
+      message: 'Continue?'
     }];
     //Ask
     modules.inquirer.prompt(prompts,
-        function (answers) {
-            if (!answers.moveon) {
-                return done();
-            }
-            if (!answers.domainName) {
-                answers.domainName = 'content';
-            }
-            common.domain.view.build(
-                answers,
-                done
-            );
-        });
-    });
-    return gulp;
+      function (answers) {
+        if (!answers.moveon) {
+          return done();
+        }
+        if (!answers.domainName) {
+          answers.domainName = 'content';
+        }
+        common.domain.view.build(
+          answers,
+          done
+          );
+      });
+  });
+  return gulp;
 }
